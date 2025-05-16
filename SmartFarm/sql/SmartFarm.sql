5111ad7726aadc5cc292cbe69fc891cd963efbf6
Khang <nldkhang@gmail.com>
2025-05-16 11:51:41 +0700

    Change sql file

--- SmartFarm/sql/SmartFarm.sql
+++ SmartFarm/sql/SmartFarm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2025 at 06:47 AM
+-- Generation Time: May 16, 2025 at 06:51 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
